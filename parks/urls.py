2880023337752b58c4c0aeb5c8a93204a80343f4
Shay <shay.phillips11@gmail.com>
2022-02-03 02:20:22 -0500

    added map view

--- parks/urls.py
+++ parks/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.ParkListView.as_view(), name='parks-list'),
     path('<slug:slug>/', views.ParkDetailView.as_view(), name='park-details'),
+    path('<slug:slug>/map/', views.map_view, name='park-map'),
 ]
--- parks/views.py
+++ parks/views.py
@@ -1,6 +1,7 @@
 from .models import Park
 from .serializers import ParkSerializer
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+from django.http import JsonResponse
 
 class ParkListView(ListAPIView):
     serializer_class = ParkSerializer
@@ -11,3 +12,16 @@ class ParkDetailView(RetrieveAPIView):
     serializer_class = ParkSerializer
     queryset = Park.objects.all()
     lookup_field = 'slug'
+
+def map_view(request, slug, *args, **kwargs):
+    data = {
+        'slug': slug
+    }
+    status = 200
+    try:
+        park = Park.objects.get(slug=slug)
+        data['map'] = park.trail_map
+    except:
+        data['message'] = 'Not found'
+        status = 404
+    return JsonResponse(data, status=status)
