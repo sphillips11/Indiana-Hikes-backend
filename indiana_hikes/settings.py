b22a326100442bae95e63c5db7370d26e048ad7b
Shay <shay.phillips11@gmail.com>
2022-02-04 16:14:29 -0500

    updated cors

--- indiana_hikes/settings.py
+++ indiana_hikes/settings.py
@@ -32,8 +32,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['ih-backend.herokuapp.com']
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
-
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
 
 # Application definition
 
@@ -45,10 +46,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'parks',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
