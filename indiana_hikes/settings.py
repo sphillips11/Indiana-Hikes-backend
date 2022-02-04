5354d072587678d71039ba0332424fddbcde8292
Shay <shay.phillips11@gmail.com>
2022-02-04 05:18:05 -0500

    add local allowed host

--- indiana_hikes/settings.py
+++ indiana_hikes/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ih-backend.herokuapp.com']
+ALLOWED_HOSTS = ['ih-backend.herokuapp.com', 'localhost:3000']
 
 
 # Application definition
