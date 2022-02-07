05ac3bad1a7752a09c380c76bd41f34cebd96399
Shay <shay.phillips11@gmail.com>
2022-02-07 03:36:56 -0500

    update Trail admin registration

--- parks/admin.py
+++ parks/admin.py
@@ -4,5 +4,8 @@ from .models import Park, Trail
 class ParkAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
+class TrailAdmin(admin.ModelAdmin):
+    list_display = ("name", "park_id")
+
 admin.site.register(Park, ParkAdmin)
-admin.site.register(Trail)
+admin.site.register(Trail, TrailAdmin)
