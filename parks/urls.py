99c89039ce58224b01039c0724453b6d0c9b2180
Shay <shay.phillips11@gmail.com>
2022-02-08 02:29:25 -0500

    fixed error in trail list url

--- parks/urls.py
+++ parks/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.ParkListView.as_view(), name='parks-list'),
     path('<slug:slug>/', views.ParkDetailView.as_view(), name='park-details'),
     path('<slug:slug>/map/', views.map_view, name='park-map'),
-    path('trails/<int:park_id>/', views.TrailListView.as_view, name="trails-by-park"),
+    path('trails/<int:park_id>/', views.TrailListView.as_view(), name="trails-by-park"),
 ]
