74938070f4eed5b4a8a2ff17ae6bce854bf9595d
Shay <shay.phillips11@gmail.com>
2022-02-11 16:20:41 -0500

    fixed Hiker model

--- hikers/models.py
+++ hikers/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Hiker(models):
+class Hiker(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
     username = models.CharField(max_length=100, unique=True)
