fd83e588d39bcee0926eb43b43f2dfd13f5e0470
Shay <shay.phillips11@gmail.com>
2022-02-13 17:13:38 -0500

    changed Hiker registered field to DateField

--- hikers/models.py
+++ hikers/models.py
@@ -6,7 +6,7 @@ class Hiker(models.Model):
     email = models.EmailField(max_length=100, unique=True)
     username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=20)
-    registered = models.DateTimeField(auto_now_add=True)
+    registered = models.DateField(auto_now_add=True)
     
     def __str__(self):
         return self.name
