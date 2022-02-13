83d2972844190be395b74d736842a8dba04943e2
Shay <shay.phillips11@gmail.com>
2022-02-12 21:44:54 -0500

    fixed hike details path

--- hikers/urls.py
+++ hikers/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.HikersView.as_view(), name="hikers"),
     path('<int:id>/', views.HikerView.as_view(), name="hiker_info"),
     path('<int:hiker_id>/hikes/', views.HikesView.as_view(), name="hikes"),
-    path('<int:hiker_id>/visits/<int:id>/', views.HikeView.as_view(), name="hike_details")
+    path('<int:hiker_id>/hikes/<int:id>/', views.HikeView.as_view(), name="hike_details")
 ]
