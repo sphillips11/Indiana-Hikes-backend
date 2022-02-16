5c34552bd0970263e0e454cd2120723e3b4042af
Shay <shay.phillips11@gmail.com>
2022-02-15 20:07:30 -0500

    moved ParkNameView

--- parks/urls.py
+++ parks/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.ParkListView.as_view(), name='parks-list'),
+    path('names/', views.ParkNameView.as_view(), name="park-names"),
     path('<slug:slug>/', views.ParkDetailView.as_view(), name='park-details'),
     path('<slug:slug>/map/', views.map_view, name='park-map'),
-    path('names/', views.ParkNameView.as_view(), name="park-names"),
     path('trails/<int:park_id>/', views.TrailListView.as_view(), name="trails-by-park"),
 ]
